5ed371a416be08f7437e964bd6fea185cd669ebb
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-11-25 17:50:29 +0200

    Add prop types to Profile component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Profile from './components/Profile';
+import Profile from './components/Profile/Profile';
 import user from './jsons/user.json';
 export default function App() {
     return (

d705e887c1cd8a12bf0dabc708ef992f17096463
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-11-27 13:02:36 +0200

    Add component statistic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Profile from './components/Profile/Profile';
 import user from './jsons/user.json';
+import Statistics from './components/Statistics/Statistics';
+import statisticalData from './jsons/statistical-data.json';
 export default function App() {
     return (
         <div>
@@ -9,6 +11,16 @@ export default function App() {
                 location={user.location}
                 avatar={user.avatar}
                 stats={user.stats}
+            />
+            <Statistics
+                title="Upload stats"
+                label={statisticalData[0].label}
+                percentage={statisticalData[0].percentage}
+            />
+            <Statistics
+                title="Upload stats"
+                label={statisticalData[1].label}
+                percentage={statisticalData[1].percentage}
             />
             ;
         </div>

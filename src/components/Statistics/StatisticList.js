45160b1f047ead75ebd339262ff7cd7d9d0cd68f
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-11-28 19:15:40 +0200

    add color generator to statistic componenet,add some style

--- src/components/Statistics/StatisticList.js
+++ src/components/Statistics/StatisticList.js
@@ -7,7 +7,11 @@ function StatisticList({ items }) {
             <h2 className={s.title}>Upload stats</h2>
             <ul className={s.statList}>
                 {items.map(item => (
-                    <li className={s.item} key={item.id}>
+                    <li
+                        className={s.item}
+                        key={item.id}
+                        style={{ backgroundColor: getColor() }}
+                    >
                         <Statistics
                             label={item.label}
                             percentage={item.percentage}
@@ -17,6 +21,13 @@ function StatisticList({ items }) {
             </ul>
         </section>
     );
+}
+function getColor() {
+    var color = Math.floor(Math.random() * Math.pow(256, 3)).toString(16);
+    while (color.length < 6) {
+        color = '0' + color;
+    }
+    return '#' + color;
 }
 
 StatisticList.propTypes = {
